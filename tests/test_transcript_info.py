6209d628add48c2e19538b6fb29ef97f883f307d
Diego Javier Zea <diegozea@gmail.com>
2019-01-17 14:51:49 +0100

    Add tests for non-coding exons

--- tests/test_transcript_info.py
+++ tests/test_transcript_info.py
@@ -7,20 +7,48 @@ from exonhomology import transcript_info
 def datadir(request):
     filename = request.module.__file__
     test_dir = os.path.dirname(filename)
-    return os.path.join(test_dir, "data")
+    return os.path.join(test_dir, 'data')
 
 
 @pytest.fixture(scope='module')
 def mapk8(datadir):
-    mapk8_dir = os.path.join(datadir, "MAPK8_ENSG00000107643")
+    mapk8_dir = os.path.join(datadir, 'MAPK8_ENSG00000107643')
     return {
         'tsl':
-        os.path.join(mapk8_dir, "TSL", "MAPK8_ENSG00000107643_TSL.csv"),
+        os.path.join(mapk8_dir, 'TSL', 'MAPK8_ENSG00000107643_TSL.csv'),
         'exontable':
-        os.path.join(mapk8_dir, "TablesExons",
-                     "MAPK8_ENSG00000107643_exonstable.tsv"),
+        os.path.join(mapk8_dir, 'TablesExons',
+                     'MAPK8_ENSG00000107643_exonstable.tsv'),
         'seqs':
-        os.path.join(mapk8_dir, "Sequences", "MAPK8_ENSG00000107643.fasta")
+        os.path.join(mapk8_dir, 'Sequences', 'MAPK8_ENSG00000107643.fasta')
+    }
+
+
+@pytest.fixture(scope='module')
+def camk2a(datadir):
+    camk2a_dir = os.path.join(datadir, 'CAMK2A_ENSG00000070808')
+    return {
+        'tsl':
+        os.path.join(camk2a_dir, 'TSL', 'CAMK2A_ENSG00000070808_TSL.csv'),
+        'exontable':
+        os.path.join(camk2a_dir, 'TablesExons',
+                     'CAMK2A_ENSG00000070808_exonstable.tsv'),
+        'seqs':
+        os.path.join(camk2a_dir, 'Sequences', 'CAMK2A_ENSG00000070808.fasta')
+    }
+
+
+@pytest.fixture(scope='module')
+def grin1(datadir):
+    grin1_dir = os.path.join(datadir, 'GRIN1_ENSG00000169258')
+    return {
+        'tsl':
+        os.path.join(grin1_dir, 'TSL', 'GRIN1_ENSG00000169258_TSL.csv'),
+        'exontable':
+        os.path.join(grin1_dir, 'TablesExons',
+                     'GRIN1_ENSG00000169258_exonstable.tsv'),
+        'seqs':
+        os.path.join(grin1_dir, 'Sequences', 'GRIN1_ENSG00000169258.fasta')
     }
 
 
@@ -57,3 +85,46 @@ def test_keep_badquality_sequences(mapk8):
 
     # ENSRNOT00000083933 has Xs in its sequence: ...VILGMGYKENGQXVXHVQRGLICC*
     assert 5 == sum(trx_data['Transcript stable ID'] == 'ENSRNOT00000083933')
+
+
+def test_non_coding_exons_camk2a(camk2a):
+    trx_data = transcript_info.read_transcript_info(
+        camk2a['tsl'], camk2a['exontable'], camk2a['seqs'], remove_na=False)
+
+    # The two first exons of ENSSSCT00000052397 are non-coding
+    assert ''.join(
+        str(exon)
+        for exon in trx_data.loc[trx_data['Transcript stable ID'] ==
+                                 'ENSSSCT00000052397', 'Exon protein sequence']
+    ) == 'MLLFLALWALVPCLVLLSLYFYSSAGGKSGGNKKNDGVKKRKSSSSVQLMESSESTNTTI'
+    'EDEDTKVRKQEIIKVTEQLIEAISNGDFESYTKMCDPGMTAFEPEALGNLVEGLDFHRFY'
+    'FENLWSRNSKPVHTTILNPHIHLMGDESACIAYIRITQYLDAGGIPRTAQSEETRVWHRR'
+    'DGKWQIVHFHRSGAPSVLPH*'
+
+
+def test_non_coding_exons_grin1(grin1):
+    trx_data = transcript_info.read_transcript_info(
+        grin1['tsl'], grin1['exontable'], grin1['seqs'], remove_na=False)
+
+    # The last two exons of ENSMUST00000099506 are non-coding.
+    # The unique coding exon has UTR at both ends.
+    assert ''.join(
+        str(exon)
+        for exon in trx_data.loc[trx_data['Transcript stable ID'] ==
+                                 'ENSSSCT00000052397', 'Exon protein sequence']
+    ) == 'MRDCCSSPKAIPAPPRHALDQSLGMDPRHTSSSGAAEGASCSERPAGSLACPSPNCSPLP'
+    'ETPRAHGALTSDNSGTTLFGKPEPMSSAEATPTASEIRNPVFSGKMDGNSLKQADSTSTR'
+    'KEEAGSLRNEESMLKGKAEPMIYGKGEPGTVGRVDCTASGAENSGSLGKVDMPCSSKVDI'
+    'VSPGGDNAGSLRKVETISSGKMDPKTENVMHSRRERPGSTGEGDLVSLRENDMKPPDNTD'
+    'SASTKKTDPEFSGKLTPGSSGKTELVSSVTVAPVTSENVNPVCSGGAGPAAVGNSETLSS'
+    'VKKDPQLLGKKEAVSSGEGGSVSVRMAETVSARQPEGMFPAKTDSTSSNSTGPSGRADPV'
+    'SLRNSELVSPVKPERLSSGQAERVSLVKTETLSSGKEDPRSSRRVDHTTVTGNMQTSQKG'
+    'NPESSGKTDLGSSSSGDTRSLGTWGSLSAAKAEVTEGKGDPQPWKKASLPASEKTDPLAS'
+    'SKAGSASQGKAETVSPGEVDAMTLGKTVPTSSGKTALVSPGKVDLMTSERAEGIPELQAS'
+    'EKGNPVNSTRVDTGATGSTEPKSGVKVITQIPGATSPGKVETPSLQKEQPQLSEKTDPSR'
+    'KVDPPTTVEPVSLGKADSASPSPRKAESQTSAKTVPQAPDKATSSLRQSDGTPYSSAQPQ'
+    'RDTRSIGSLPEREPSASTSQKDLAAAAAQKSPSAEAAAPPPGPRTRDNFTKAPSWDAGAP'
+    'PPREDAGTQAGAQACVSVAVSPMSPQDGAGGPAFSFQAAPRAPSPAPRPPSRRDAGLQVS'
+    'LGAAETRSVATGPMTPQAAAPPAVPPVFPEVRVRPGSVLAAALAPQEATEPVRDVSWDEK'
+    'GMTWEVYGASMEVEVLGMAIQKHLERQIEEHGRQGAPAPAPPPAVRAGPGRAGSVRTAPA'
+    'EGAAKRPPGLFRALLQSVRRPRCCSRAGPTAE*'
