409b51bdaf80d662648f09f7759430f2614e45b2
Diego Javier Zea <diegozea@gmail.com>
2019-09-30 19:57:56 +0200

    Do not test with cricetulus_griseus_crigri and Update docs

--- tests/test_transcript_query.py
+++ tests/test_transcript_query.py
@@ -33,7 +33,7 @@ def test_download(monkeypatch):
             'homo_sapiens,mus_musculus,pan_troglodytes,'
             'panthera_tigris_altaica,cebus_capucinus_imitator,'
             'cricetulus_griseus_chok1gshd,'  # colobus_angolensis_palliatus,
-            'cricetulus_griseus_crigri,canis_lupus_dingo,mustela_putorius_furo,'
+            'canis_lupus_dingo,mustela_putorius_furo,'  # cricetulus_griseus_crigri,
             'heterocephalus_glaber_female,heterocephalus_glaber_male'
         ])
     assert not os.path.isdir('MAPK8')
