bd9edb602e6ec96dccdcd7423c7912e03c69629b
Diego Javier Zea <diegozea@gmail.com>
2019-09-30 20:05:27 +0200

    Do not test cricetulus_griseus_chok1gshd

--- tests/test_transcript_query.py
+++ tests/test_transcript_query.py
@@ -32,7 +32,8 @@ def test_download(monkeypatch):
             '-l',
             'homo_sapiens,mus_musculus,pan_troglodytes,'
             'panthera_tigris_altaica,cebus_capucinus_imitator,'
-            'cricetulus_griseus_chok1gshd,'  # colobus_angolensis_palliatus,
+            # 'cricetulus_griseus_chok1gshd,'
+            # 'colobus_angolensis_palliatus,'
             'canis_lupus_dingo,mustela_putorius_furo,'  # cricetulus_griseus_crigri,
             'heterocephalus_glaber_female,heterocephalus_glaber_male'
         ])
