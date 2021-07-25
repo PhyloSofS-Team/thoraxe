f6e1e923373949ec98662fe94d7741cff9a3f7d8
Diego Javier Zea <diegozea@gmail.com>
2021-07-25 18:11:23 +0200

    Use echo to debug doc creation

--- push_docs.sh
+++ push_docs.sh
@@ -16,22 +16,22 @@ EMAIL=diegozea@gmail.com
 
 set -e
 
-# Clone the gh-pages branch outside of the repo and cd into it.
+echo "Clone the gh-pages branch outside of the repo and cd into it."
 cd ..
 git clone -b gh-pages "https://$GH_TOKEN@github.com/$ORG/$REPO.git" gh-pages
 cd gh-pages
 
-# Update git configuration so I can push.
+echo "Update git configuration so I can push."
 if [ "$1" != "dry" ]; then
     # Update git config.
     git config user.name "Travis Builder"
     git config user.email "$EMAIL"
 fi
 
-# Copy in the HTML.  You may want to change this with your documentation path.
+echo "Copy in the HTML.  You may want to change this with your documentation path."
 cp -R ../$REPO/docs/build/html/* ./
 
-# Add and commit changes.
+echo "Add and commit changes."
 git add -A .
 git commit -m "[ci skip] Autodoc commit for $COMMIT."
 if [ "$1" != "dry" ]; then
